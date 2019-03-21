b76f270b738a00186ebf4b342df62e612be9b930
Wofluently <f_liuchang@163.com>
2019-03-21 11:30:29 +0800

    添加加载进度条

--- src/component/blog/List.js
+++ src/component/blog/List.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import { List } from "antd-mobile";
+import { List, Progress, ActivityIndicator } from "antd-mobile";
 import { timeConvert } from "../../util/time";
 const Item = List.Item;
 const Brief = Item.Brief;
@@ -8,7 +8,9 @@ export default class BlogList extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      blogList: []
+      blogList: [],
+      percent: 0,
+      showPercent: true
     };
     this.handleClick = this.handleClick.bind(this);
   }
@@ -18,28 +20,46 @@ export default class BlogList extends Component {
   }
 
   componentWillMount() {
+    let interval = setInterval(() => {
+      this.setState((prevState, props) => ({
+        percent: prevState.percent + 20
+      }));
+    }, 100);
     this.postRequestBody("/blog/getMyBlog", {}).then(res => {
+      clearInterval(interval);
       this.setState({
-        blogList: res
+        blogList: res,
+        percent: 100
       });
+      setTimeout(() => {
+        this.setState({
+          showPercent: false
+        });
+      }, 500);
     });
   }
 
   render() {
     return (
-      <List renderHeader={() => "博客列表"} className="my-list">
-        {this.state.blogList.map(item => (
-          <Item
-            key={item.id}
-            extra={"访客:" + item.vistor_count}
-            align="top"
-            multipleLine
-            onClick={e => this.handleClick(item)}
-          >
-            {item.title} <Brief>{timeConvert(item.time)}</Brief>
-          </Item>
-        ))}
-      </List>
+      <>
+        {this.state.showPercent && (
+          <Progress percent={this.state.percent} position="fixed" />
+        )}
+
+        <List renderHeader={() => "博客列表"} className="my-list">
+          {this.state.blogList.map(item => (
+            <Item
+              key={item.id}
+              extra={"访客:" + item.vistor_count}
+              align="top"
+              multipleLine
+              onClick={e => this.handleClick(item)}
+            >
+              {item.title} <Brief>{timeConvert(item.time)}</Brief>
+            </Item>
+          ))}
+        </List>
+      </>
     );
   }
 }
