5e6a7d505aa992673363706c7534ca750dc607b2
Wofluently <f_liuchang@163.com>
2019-03-22 10:11:29 +0800

    本地代理配置

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -5,6 +5,8 @@ module.exports = function(app) {
       target: "https://www.coffeecola.cn:8080/",
       changeOrigin: true, // needed for virtual hosted sites
       ws: true, // proxy websockets
+      // 代理本地配置此配置，取消SSL校验 
+      // secure: false,
       pathRewrite: {
         "^/api": ""
       }
